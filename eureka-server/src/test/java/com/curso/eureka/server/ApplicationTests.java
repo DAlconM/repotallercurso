72161dc6af1989e38b25d26e1474c1af7d07e072
DAlconM <daniam97@hotmail.com>
2020-11-26 08:43:59 +0100

    Añadidos cloud y eureka server
    Se han añadido los servidores de eureka y cloud

---
+++ eureka-server/src/test/java/com/curso/eureka/server/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.curso.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
